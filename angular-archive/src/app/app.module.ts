9435befa62943fb21a633a8e6182970dce6f991a
OzPro33700 <osiris.malbranque@fr.thalesgroup.com>
2022-07-26 15:13:39 +0200

    1 file modified, 9 added

--- angular-archive/src/app/app.module.ts
+++ angular-archive/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule
